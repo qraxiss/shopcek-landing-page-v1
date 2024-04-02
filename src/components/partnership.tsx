4ae33129567db471a51157fffdd42f4e05c9a6dd
qraxiss <qraxiss@gmail.com>
2024-04-02 20:25:15 +0300

    build

--- src/components/partnership.tsx
+++ src/components/partnership.tsx
@@ -2,54 +2,66 @@ import React from 'react'
 
 import handshake from '../assets/images/partnership/handshake.png'
 
+import { useApply, ApplyProvider } from '../assets/context/apply'
+
 export function Partnership() {
+    const { sendInformationGQL } = useApply()
+
+    function Component() {
+        return (
+            <React.Fragment>
+                <section className="partnership-page">
+                    <div className="partnership" id="partnership">
+                        <div className="info">
+                            <div className="title">PARTNERSHIP</div>
+
+                            <div className="sub-title">YOUR AMBITION, OUR DIGITAL EXECUTION</div>
+
+                            <hr />
+
+                            <div className="text">
+                                Welcome to the Shopcek partner program, where we fuse innovation and opportunity to create unique online storefronts
+                                for WEB3 projects and influencers.
+                                <br /> <br />
+                                Our mission is to transform your online presence into a dynamic e-commerce store, enriched with tools and services
+                                tailored to enhance your brand.
+                            </div>
+                        </div>
+
+                        <div className="apply">
+                            <div className="title">Apply The Program</div>
+
+                            <div className="form">
+                                <div className="row">
+                                    <input type="text" placeholder="PARTNER NAME" />
+                                    <input type="text" placeholder="EMAIL" />
+                                </div>
+
+                                <div className="row">
+                                    <input type="text" placeholder="CONTACT NAME" />
+                                    <input type="text" placeholder="TELEGRAM HANDLE" />
+                                </div>
+
+                                <button className="submit">
+                                    <div className="text">SUBMIT</div>
+                                </button>
+                            </div>
+
+                            <div className="info-text">
+                                *Once we've reviewed your application, our team will be in touch to discuss the next steps. Thank you for reaching out
+                                to us.
+                            </div>
+                        </div>
+                    </div>
+                    <img src={handshake} className="handshake" />
+                </section>
+            </React.Fragment>
+        )
+    }
+
     return (
-        <React.Fragment>
-            <section className="partnership-page">
-                <div className="partnership" id="partnership">
-                    <div className="info">
-                        <div className="title">PARTNERSHIP</div>
-
-                        <div className="sub-title">YOUR AMBITION, OUR DIGITAL EXECUTION</div>
-
-                        <hr />
-
-                        <div className="text">
-                            Welcome to the Shopcek partner program, where we fuse innovation and opportunity to create unique online storefronts for
-                            WEB3 projects and influencers.
-                            <br /> <br />
-                            Our mission is to transform your online presence into a dynamic e-commerce store, enriched with tools and services
-                            tailored to enhance your brand.
-                        </div>
-                    </div>
-
-                    <div className="apply">
-                        <div className="title">Apply The Program</div>
-
-                        <div className="form">
-                            <div className="row">
-                                <input type="text" placeholder="PARTNER NAME" />
-                                <input type="text" placeholder="EMAIL" />
-                            </div>
-
-                            <div className="row">
-                                <input type="text" placeholder="CONTACT NAME" />
-                                <input type="text" placeholder="TELEGRAM HANDLE" />
-                            </div>
-
-                            <button className="submit">
-                                <div className="text">SUBMIT</div>
-                            </button>
-                        </div>
-
-                        <div className="info-text">
-                            *Once we've reviewed your application, our team will be in touch to discuss the next steps. Thank you for reaching out to
-                            us.
-                        </div>
-                    </div>
-                </div>
-                <img src={handshake} className="handshake" />
-            </section>
-        </React.Fragment>
+        <ApplyProvider>
+            <Component />
+        </ApplyProvider>
     )
 }
