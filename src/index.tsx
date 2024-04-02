ae128180324413931d344a6db45d1f8054462d35
qraxiss <qraxiss@gmail.com>
2024-04-02 18:21:56 +0300

    build

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <App />
-        {/* <div className="bg"></div> */}
+        <div className="bg"></div>
     </React.StrictMode>
 )
 
