a395a6843eb42984827393c179aeb739ef051938
qraxiss <qraxiss@gmail.com>
2024-04-03 17:06:31 +0300

    build

--- src/context/menu/index.tsx
+++ src/context/menu/index.tsx
@@ -1,1 +1,11 @@
 import { createContext, useContext, useEffect, useState } from 'react'
+import { Menu } from './types'
+
+//@ts-ignore
+const MenuContext = createContext<Menu>()
+
+export function MenuProvider({ children }: { children: any }) {
+    const [open, setOpen] = useState(false)
+
+    return <MenuContext.Provider value={{ open, setOpen }}>{children}</MenuContext.Provider>
+}
--- src/context/menu/types.d.ts
+++ src/context/menu/types.d.ts
@@ -1,3 +1,4 @@
 export type Menu = {
     open: boolean
+    setOpen: (value: boolean) => void
 }
