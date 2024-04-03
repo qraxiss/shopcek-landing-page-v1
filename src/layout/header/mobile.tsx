4b20b3426c791548e79f1745a716d8bf6b930904
qraxiss <qraxiss@gmail.com>
2024-04-03 17:12:49 +0300

    build

--- src/layout/header/mobile.tsx
+++ src/layout/header/mobile.tsx
@@ -16,7 +16,7 @@ export function Header() {
 
         return (
             <React.Fragment>
-                <Menu open={open} onClick={console.log}>
+                <Menu open={open}>
                     <div className="menu">
                         <Logo />
                         <hr />
